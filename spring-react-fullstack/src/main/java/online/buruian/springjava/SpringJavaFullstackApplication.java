ee455224a12a66ff3271bab1b3598fd7be1cdb67
Valeriu Buruian <valeraburuian@mail.ru>
2019-06-04 11:16:09 +0300

    first commit

---
+++ spring-react-fullstack/src/main/java/online/buruian/springjava/SpringJavaFullstackApplication.java
@@ -0,0 +1,13 @@
+package online.buruian.springjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJavaFullstackApplication.class, args);
+    }
+
+}
---
+++ spring-react-fullstack/src/test/java/online/buruian/springjava/SpringJavaFullstackApplicationTests.java
@@ -0,0 +1,16 @@
+package online.buruian.springjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJavaFullstackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
